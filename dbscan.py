b635103914c86cc27759698010e82276a1600812
Zeyu Lu <11510076@mail.sustc.edu.cn>
2018-07-15 22:49:38 -0400

    reformat
    reformat

--- dbscan.py
+++ dbscan.py
@@ -7,6 +7,7 @@ This is a simple implementation of DBSCAN intended to explain the algorithm.
 
 import numpy
 
+
 def MyDBSCAN(D, eps, MinPts):
     """
     Cluster the dataset `D` using the DBSCAN algorithm.
@@ -17,35 +18,35 @@ def MyDBSCAN(D, eps, MinPts):
     It will return a list of cluster labels. The label -1 means noise, and then
     the clusters are numbered starting from 1.
     """
- 
+
     # This list will hold the final cluster assignment for each point in D.
     # There are two reserved values:
     #    -1 - Indicates a noise point
     #     0 - Means the point hasn't been considered yet.
     # Initially all labels are 0.    
-    labels = [0]*len(D)
+    labels = [0] * len(D)
 
     # C is the ID of the current cluster.    
     C = 0
-    
+
     # This outer loop is just responsible for picking new seed points--a point
     # from which to grow a new cluster.
     # Once a valid seed point is found, a new cluster is created, and the 
     # cluster growth is all handled by the 'expandCluster' routine.
-    
+
     # For each point P in the Dataset D...
     # ('P' is the index of the datapoint, rather than the datapoint itself.)
     for P in range(0, len(D)):
-    
+
         # Only points that have not already been claimed can be picked as new 
         # seed points.    
         # If the point's label is not 0, continue to the next point.
         if not (labels[P] == 0):
-           continue
-        
+            continue
+
         # Find all of P's neighboring points.
         NeighborPts = regionQuery(D, P, eps)
-        
+
         # If the number is below MinPts, this point is noise. 
         # This is the only condition under which a point is labeled 
         # NOISE--when it's not a valid seed point. A NOISE point may later 
@@ -56,10 +57,10 @@ def MyDBSCAN(D, eps, MinPts):
             labels[P] = -1
         # Otherwise, if there are at least MinPts nearby, use this point as the 
         # seed for a new cluster.    
-        else: 
-           C += 1
-           growCluster(D, labels, P, NeighborPts, C, eps, MinPts)
-    
+        else:
+            C += 1
+            growCluster(D, labels, P, NeighborPts, C, eps, MinPts)
+
     # All data has been clustered!
     return labels
 
@@ -83,7 +84,7 @@ def growCluster(D, labels, P, NeighborPts, C, eps, MinPts):
 
     # Assign the cluster label to the seed point.
     labels[P] = C
-    
+
     # Look at each neighbor of P (neighbors are referred to as Pn). 
     # NeighborPts will be used as a FIFO queue of points to search--that is, it
     # will grow as we discover new branch points for the cluster. The FIFO
@@ -91,39 +92,39 @@ def growCluster(D, labels, P, NeighborPts, C, eps, MinPts):
     # In NeighborPts, the points are represented by their index in the original
     # dataset.
     i = 0
-    while i < len(NeighborPts):    
-        
+    while i < len(NeighborPts):
+
         # Get the next point from the queue.        
         Pn = NeighborPts[i]
-       
+
         # If Pn was labelled NOISE during the seed search, then we
         # know it's not a branch point (it doesn't have enough neighbors), so
         # make it a leaf point of cluster C and move on.
         if labels[Pn] == -1:
-           labels[Pn] = C
-        
+            labels[Pn] = C
+
         # Otherwise, if Pn isn't already claimed, claim it as part of C.
         elif labels[Pn] == 0:
             # Add Pn to cluster C (Assign cluster label C).
             labels[Pn] = C
-            
+
             # Find all the neighbors of Pn
             PnNeighborPts = regionQuery(D, Pn, eps)
-            
+
             # If Pn has at least MinPts neighbors, it's a branch point!
             # Add all of its neighbors to the FIFO queue to be searched. 
             if len(PnNeighborPts) >= MinPts:
                 NeighborPts = NeighborPts + PnNeighborPts
             # If Pn *doesn't* have enough neighbors, then it's a leaf point.
             # Don't queue up it's neighbors as expansion points.
-            #else:
-                # Do nothing                
-                #NeighborPts = NeighborPts               
-        
+            # else:
+            # Do nothing
+            # NeighborPts = NeighborPts
+
         # Advance to the next point in the FIFO queue.
-        i += 1        
-    
-    # We've finished growing cluster C!
+        i += 1
+
+        # We've finished growing cluster C!
 
 
 def regionQuery(D, P, eps):
@@ -135,12 +136,12 @@ def regionQuery(D, P, eps):
     threshold distance `eps`.
     """
     neighbors = []
-    
+
     # For each point in the dataset...
     for Pn in range(0, len(D)):
-        
+
         # If the distance is below the threshold, add it to the neighbors list.
         if numpy.linalg.norm(D[P] - D[Pn]) < eps:
-           neighbors.append(Pn)
-            
+            neighbors.append(Pn)
+
     return neighbors
