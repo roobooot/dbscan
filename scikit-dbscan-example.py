5d067fdae485949faaf0d4b6d19d3570dae5fbb1
Zeyu Lu <11510076@mail.sustc.edu.cn>
2018-07-15 22:02:32 -0400

    solve print issues

--- scikit-dbscan-example.py
+++ scikit-dbscan-example.py
@@ -28,14 +28,14 @@ X = StandardScaler().fit_transform(X)
 #
 
 # Run my DBSCAN implementation.
-print 'Running my implementation...'
+print('Running my implementation...')
 my_labels = MyDBSCAN(X, eps=0.3, MinPts=10)
 
 ###############################################################################
 # Scikit-learn implementation of DBSCAN
 #
 
-print 'Runing scikit-learn implementation...'
+print ('Runing scikit-learn implementation...')
 db = DBSCAN(eps=0.3, min_samples=10).fit(X)
 skl_labels = db.labels_
 
@@ -55,10 +55,10 @@ num_disagree = 0
 # don't)
 for i in range(0, len(skl_labels)):
     if not skl_labels[i] == my_labels[i]:
-        print 'Scikit learn:', skl_labels[i], 'mine:', my_labels[i]
+        print ('Scikit learn:', skl_labels[i], 'mine:', my_labels[i])
         num_disagree += 1
 
 if num_disagree == 0:
-    print 'PASS - All labels match!'
+    print ('PASS - All labels match!')
 else:
-    print 'FAIL -', num_disagree, 'labels don\'t match.'        
+    print ('FAIL -', num_disagree, 'labels don\'t match.')
